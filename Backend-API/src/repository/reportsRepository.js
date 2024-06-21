29fadd12746db2c3ea29a4d9b71f9fd6a1a341df
RafaSantos19 <88862178+RafaSantos19@users.noreply.github.com>
2024-06-21 11:56:28 -0300

    Mostrar Relatórios
    Tentei mostrar as informações em um modal dedicado a cada tipo de relatório, mas pra variar as informações não estava indo.
    
    O que foi Alterado:
    Profile.html - botão dos modal
    reports.js - Função de display foi construída, mas as informações não são carregadas

--- Backend-API/src/repository/reportsRepository.js
+++ Backend-API/src/repository/reportsRepository.js
@@ -34,7 +34,31 @@ class ReportsRepository {
           FROM SCHEDULE AS s 
           JOIN USER AS u ON u.id = s.userId
           JOIN SERVICES AS se ON se.id = s.serviceId
+<<<<<<< Updated upstream
           WHERE s.status = "1" LIMIT 100
+=======
+          WHERE s.status = "1" AND s.date = "${date}" LIMIT 100
+        `
+      ).then((res) => res).catch(err => { throw new Error("Erro ao buscar agendamentos") });
+    });
+  }
+
+  async trending() {
+    return openDb().then((db) => {
+      return db.all(
+        `
+          SELECT 
+          s.name AS serviceName,
+          COUNT(sc.id) AS totalSchedules
+          FROM 
+          SCHEDULE sc
+          JOIN 
+          SERVICES s ON sc.serviceId = s.id
+          GROUP BY 
+          sc.serviceId
+          ORDER BY 
+          totalSchedules DESC LIMIT 100
+>>>>>>> Stashed changes
         `
       ).then((res) => res).catch(err => { throw new Error("Erro ao buscar agendamentos") });
     });
--- Frontend-API/src/js/reports.js
+++ Frontend-API/src/js/reports.js
@@ -17,20 +17,89 @@ document.getElementById('relatorio1').addEventListener('click', async (event) =>
             throw new Error('Erro ao buscar agendamentos');
         }
 
-        const list = await response.json();
-        console.log(list);
+        const lasts = await response.json();
+        console.log(lasts);
     } catch (error) {
         console.error('Erro:', error);
     }
 });
 
+function displayLasts(lasts){
+    const container = document.getElementById('lastsContainer');
+    container.innerHTML = '';
+
+    if(lasts.length === 0){
+        container.innerHTML = '<p>Nenhum agendamento anterior encontrado</p>';
+        return;
+    }
+
+    lasts.forEach(lasts => {
+        const lastsDiv = document.createElement('div');
+        lastsDiv.classList.add('flex', 'items-center', 'w-full', 'p-3', 'py-1', 'pl-4', 'pr-1', 'leading-tight', 'transition-all', 'rounded-lg', 'outline-none', 'text-start', 'hover:bg-blue-gray-50', 'hover:bg-opacity-80', 'hover:text-blue-gray-900', 'focus:bg-blue-gray-50', 'focus:bg-opacity-80', 'focus:text-blue-gray-900', 'active:bg-blue-gray-50', 'active:bg-opacity-80', 'active:text-blue-gray-900')
+        
+        lastsDiv.innerHTML = `
+          ${lasts.serviceName} <p class='ml-auto'>${lasts.date} ${lasts.time} ${lasts.userName} ${lasts.userPhone}</p>  
+        `;
+
+        container.appendChild(lastsDiv);
+    
+    })
+}
+
 document.getElementById('relatorio2').addEventListener('click', async (event) => {
     alert('relatorio2')
 });
 
+<<<<<<< Updated upstream
 document.getElementById('relatorio3').addEventListener('click', async (event) => {
     alert('relatorio3')
+=======
+document.addEventListener('DOMContentLoaded', (event) => {
+    // Mostrar botão quando a página carregar
+    document.getElementById('relatorio3').style.display = 'block';
+
+    // Adicionar evento de clique ao botão
+    document.getElementById('relatorio3').addEventListener('click', async (event) => {
+        try {
+            const response = await fetch('http://localhost:3000/reports/trending', {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+            });
+
+            if (!response.ok) {
+                throw new Error('Erro ao buscar agendamentos');
+            }
+
+            const trending = await response.json();
+            console.log(trending);
+            displayTrending(trending);
+        } catch (error) {
+            console.error('Erro:', error);
+        }
+    });
+>>>>>>> Stashed changes
 });
+
+function displayTrending(trending) {
+    const container = document.getElementById('trendingContainer');
+    container.innerHTML = '';
+
+    if (trending.length === 0) {
+        container.innerHTML = '<p>Nenhum agendamento encontrado</p>';
+        return;
+    }
+
+    trending.forEach(item => {
+        const trendingDiv = document.createElement('div');
+        trendingDiv.classList.add('flex', 'items-center', 'w-full', 'p-3', 'py-1', 'pl-4', 'pr-1', 'leading-tight', 'transition-all', 'rounded-lg', 'outline-none', 'text-start', 'hover:bg-blue-gray-50', 'hover:bg-opacity-80', 'hover:text-blue-gray-900', 'focus:bg-blue-gray-50', 'focus:bg-opacity-80', 'focus:text-blue-gray-900', 'active:bg-blue-gray-50', 'active:bg-opacity-80', 'active:text-blue-gray-900');
+
+        trendingDiv.innerHTML = `<p class='ml-auto'>${item.serviceName} = ${item.totalSchedules}</p>`;
+        container.appendChild(trendingDiv);
+    });
+}
+
 
 // TODO: Gerar PDF do relatório
 // function generatePDF(list) {
